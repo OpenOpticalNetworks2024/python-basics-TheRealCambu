fa717bd022e1beef2204f67879857ab6fd5f6900
TheRealCambu <s324564@studenti.polito.it>
2024-10-27 23:59:15 +0100

    Arrived to exercise 2

--- Lab1/exercise_set3.py
+++ Lab1/exercise_set3.py
@@ -6,12 +6,21 @@ import matplotlib as plt
 
 # ex1
 def exercise1():
-    pass
+    random_integers = np.random.randint(0, 10, size=(4, 2), dtype=np.uint8)
+    print("Array:\n", random_integers)
+
+    print("Attributes:")
+    print("Shape:", random_integers.shape)
+    print("Size:", random_integers.size)
+    print("Data type:", random_integers.dtype)
+    print("Number of dimensions:", random_integers.ndim)
+    print("Memory size (bytes):", random_integers.nbytes)
 
 
 # ex2
 def exercise2():
-    pass
+    random_vect = np.arange(100, 200, 10).reshape((5, 2))
+    print(random_vect)
 
 
 # ex3
@@ -55,9 +64,9 @@ def exercise10():
 
 
 if __name__ == "__main__":
-    print("EXERCISE SET 3: NumPy Exercises")
-    print("EX1")
-    exercise1()
+    # print("EXERCISE SET 3: NumPy Exercises")
+    # print("EX1")
+    # exercise1()
 
     print("EX2")
     exercise2()
