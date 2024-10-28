b463051271d8880d503a4d7dc34117961d9dc358
TheRealCambu <s324564@studenti.polito.it>
2024-10-28 18:51:38 +0100

    Done with Lab2!

--- Lab2/exercise_set2.py
+++ Lab2/exercise_set2.py
@@ -65,7 +65,6 @@ def exercise5(json_data, key_to_del):
     for state in json_data.get("states", []):
         if key_to_del in state:
             del state[key_to_del]
-            print(f"Deleted 'area_codes' from {state['name']}.")
 
     return json_data
 
@@ -78,37 +77,37 @@ if __name__ == "__main__":
         'Age': 6
     }
 
-    print("EXERCISE SET 1")
-    print("EX1")
-    converted_py_obj = exercise1(json_obj)
-    print(f"Original JSON:\n", {json_obj})
-    print("Converted into Python dictionary:\n", converted_py_obj)
+    # print("EXERCISE SET 1")
+    # print("EX1")
+    # converted_py_obj = exercise1(json_obj)
+    # print(f"Original JSON:\n", {json_obj})
+    # print("Converted into Python dictionary:\n", converted_py_obj)
+    #
+    # print("EX2")
+    # converted_json_obj = exercise2(python_obj)
+    # print("Original JSON data:\n", json_obj)
+    # print("Converted into Python dictionary:\n", converted_json_obj)
+    #
+    # print("EX3")
+    # JSON_strings = exercise3(python_obj)
+    # print(f"Original Python object:\n", {json_obj})
+    # print("Printing JSON strings...")
+    # for idx, string in enumerate(JSON_strings):
+    #     print(f"String {idx + 1}: {string}")
+    #
+    # print("EX4")
+    # indent_level = 4
+    # indentation = " " * 4
+    # converted_json_obj = exercise4(python_obj, indentation)
+    # print(f"Original Python object:\n{python_obj}")
+    # print(f"Converted into JSON data with indent level {indent_level}:\n{converted_json_obj}")
 
-    print("EX2")
-    converted_json_obj = exercise2(python_obj)
-    print("Original JSON data:\n", json_obj)
-    print("Converted into Python dictionary:\n", converted_json_obj)
-
-    print("EX3")
-    JSON_strings = exercise3(python_obj)
-    print(f"Original Python object:\n", {json_obj})
-    print("Printing JSON strings...")
-    for idx, string in enumerate(JSON_strings):
-        print(f"String {idx + 1}: {string}")
-
-    print("EX4")
-    indent_level = 4
-    indentation = " " * 4
-    converted_json_obj = exercise4(python_obj, indentation)
-    print(f"Original Python object:\n{python_obj}")
-    print(f"Converted into JSON data with indent level {indent_level}:\n{converted_json_obj}")
-
-    print("EX5")
-    with open("states.json") as file:
-        data_json = json.load(file)
-    key_to_delete = "area_codes"
-    new_json_fpath = "states_no_area_code.json"
-    new_json_data_area_code = exercise5(data_json, key_to_delete)
-    with open(new_json_fpath, "w") as file:
-        json.dump(new_json_data_area_code, file, indent=2)
-    print("'area_codes' field removed and the new JSON file has been saved!")
+    # print("EX5")
+    # with open("states.json") as file:
+    #     data_json = json.load(file)
+    # key_to_delete = "area_codes"
+    # new_json_fpath = "states_no_area_code.json"
+    # new_json_data_area_code = exercise5(data_json, key_to_delete)
+    # with open(new_json_fpath, "w") as file:
+    #     json.dump(new_json_data_area_code, file, indent=2)
+    # print("'area_codes' field removed and the new JSON file has been saved!")
