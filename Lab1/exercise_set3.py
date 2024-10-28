bf952d8bf496389b19716e83b6ad2afa93121137
TheRealCambu <s324564@studenti.polito.it>
2024-10-28 01:11:35 +0100

    Finished with Lab1

--- Lab1/exercise_set3.py
+++ Lab1/exercise_set3.py
@@ -39,61 +39,65 @@ def exercise5(list_of_lists1, list_of_lists2):
 
 
 # ex6
-def exercise6():
-    pass
+def exercise6(list_of_lists, axis=-1):
+    if axis not in [None, -1, 0, 1]:
+        raise ValueError("The inserted mode for \"np.sort\" is not valid!")
+    return np.sort(list_of_lists, axis=axis)
 
 
 # ex7
-def exercise7():
-    pass
+def exercise7(list_of_lists):
+    return np.max(list_of_lists, axis=0), np.min(list_of_lists, axis=1)
 
 
 # ex8
-def exercise8():
-    pass
-
-
-# ex9
-def exercise9():
-    pass
-
-
-# ex10
-def exercise10():
-    pass
+def exercise8(list_of_lists, new_col):
+    temp = np.delete(list_of_lists, 1, axis=1)
+    new_col = new_col.reshape(-1, 1)
+    return np.hstack((temp[:, :1], new_col, temp[:, 1:]))
 
 
 if __name__ == "__main__":
-    # print("EXERCISE SET 3: NumPy Exercises")
-    # print("EX1")
-    # exercise1()
+    print("EXERCISE SET 3: NumPy Exercises")
+    print("EX1")
+    exercise1()
 
-    # print("EX2")
-    # exercise2()
+    print("EX2")
+    exercise2()
 
-    # print("EX3")
-    # l_o_l = np.array([[11, 22, 33], [44, 55, 66], [77, 88, 99]])
-    # output = exercise3(l_o_l)
-    # print(output)
+    print("EX3")
+    l_o_l = np.array([[11, 22, 33], [44, 55, 66], [77, 88, 99]])
+    output = exercise3(l_o_l)
+    print(output)
 
-    # print("EX4")
-    # l_o_l = np.array([[3, 6, 9, 12], [15, 18, 21, 24], [27, 30, 33, 36], [39, 42, 45, 48], [51, 54, 57, 60]])
-    # output = exercise4(l_o_l)
-    # print(output)
+    print("EX4")
+    l_o_l = np.array([[3, 6, 9, 12], [15, 18, 21, 24], [27, 30, 33, 36], [39, 42, 45, 48], [51, 54, 57, 60]])
+    output = exercise4(l_o_l)
+    print(output)
 
-    # print("EX5")
-    # l_o_l_1 = np.array([[5, 6, 9], [21, 18, 27]])
-    # l_o_l_2 = np.array([[15, 33, 24], [4, 7, 1]])
-    # output = exercise5(l_o_l_1, l_o_l_2)
-    # print("Array 1:\n", l_o_l_1)
-    # print("Array 2:\n", l_o_l_2)
-    # print("Sum of np arrays and then apply sqrt:\n", output)
+    print("EX5")
+    l_o_l_1 = np.array([[5, 6, 9], [21, 18, 27]])
+    l_o_l_2 = np.array([[15, 33, 24], [4, 7, 1]])
+    output = exercise5(l_o_l_1, l_o_l_2)
+    print("Array 1:\n", l_o_l_1)
+    print("Array 2:\n", l_o_l_2)
+    print("Sum of np arrays and then apply sqrt:\n", output)
 
     print("EX6")
-    exercise6()
+    l_o_l = np.array([[34, 43, 73], [82, 22, 12], [53, 94, 66]])
+    output = exercise6(l_o_l, None)
+    print(output)
 
     print("EX7")
-    exercise7()
+    l_o_l = np.array([[34, 43, 73], [82, 22, 12], [53, 94, 66]])
+    max_a0, min_a1 = exercise7(l_o_l)
+    print("The array is:\n", l_o_l)
+    print("The maximum on axis 0 (columns) is:", max_a0)
+    print("The minimum on axis 1 (rows) is:", min_a1)
 
     print("EX8")
-    exercise8()
+    l_o_l = np.array([[34, 43, 73], [82, 22, 12], [53, 94, 66]])
+    new_column = np.array([10, 10, 10])
+    new_l_o_l = exercise8(l_o_l, new_column)
+    print("Original Numpy array:\n", l_o_l)
+    print(f"The new array with the new column {new_column} is:\n{new_l_o_l}")
