8fb473e9f3a1596b46329787c38b8be46d6ec31e
TheRealCambu <s324564@studenti.polito.it>
2024-10-28 17:47:07 +0100

    Done with Lab2!

--- Lab2/exercise_set2.py
+++ Lab2/exercise_set2.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import re
+import json
 
 
 # Convert JSON data to Python objects
@@ -24,7 +25,7 @@ def exercise2(py_dict):
     len_dict = len(py_dict)
     json_object = "{ "
     for index, (key, value) in enumerate(py_dict.items()):
-        json_object += "\"" + key + "\":" + "\"" + str(value) + "\""
+        json_object += f'"{key}": "{value}"'
         if index != len_dict - 1:
             json_object += ", "
         else:
@@ -37,15 +38,36 @@ def exercise3(py_dict):
     if not isinstance(py_dict, dict):
         raise TypeError(f"Expected dict for 'py_dict', got {type(py_dict).__name__}")
 
-    return ["\"" + key + "\": \"" + str(value) + "\"" for key, value in py_dict.items()]
+    return [f'"{key}": "{value}"' for key, value in py_dict.items()]
 
 
-def exercise4():
-    pass
+# Write a Python program to convert Python dictionary objects (sort by key) to JSON data.
+# Print the object members with indent level 4.
+def exercise4(py_dict, indent):
+    if not isinstance(py_dict, dict):
+        raise TypeError(f"Expected dict for 'py_dict', got {type(py_dict).__name__}")
+
+    # Sort by the keys
+    ordered_py_dict = dict(sorted(py_dict.items()))
+    len_dict = len(ordered_py_dict)
+    json_object = "{\n"
+    for index, (key, value) in enumerate(ordered_py_dict.items()):
+        json_object += f'{indent}"{key}": "{value}"'
+        if index != len_dict - 1:
+            json_object += ",\n"
+        else:
+            json_object += "\n"
+    json_object += "}"  # Close the JSON object
+    return json_object
 
 
-def exercise5():
-    pass
+def exercise5(json_data, key_to_del):
+    for state in json_data.get("states", []):
+        if key_to_del in state:
+            del state[key_to_del]
+            print(f"Deleted 'area_codes' from {state['name']}.")
+
+    return json_data
 
 
 if __name__ == "__main__":
@@ -74,8 +96,20 @@ if __name__ == "__main__":
     # for idx, string in enumerate(JSON_strings):
     #     print(f"String {idx + 1}: {string}")
 
-    print("EX4")
-    exercise4()
+    # print("EX4")
+    # indent_level = 4
+    # indentation = " " * 4
+    # converted_json_obj = exercise4(python_obj, indentation)
+    # print(f"Original Python object:\n{python_obj}")
+    # print(f"Converted into JSON data with indent level {indent_level}:\n{converted_json_obj}")
 
     print("EX5")
-    exercise5()
+    with open("states.json") as file:
+        data_json = json.load(file)
+    key_to_delete = "area_codes"
+    new_json_fpath = "states_no_area_code.json"
+    new_json_data_area_code = exercise5(data_json, key_to_delete)
+    with open(new_json_fpath, "w") as file:
+        json.dump(new_json_data_area_code, file, indent=2)
+
+    print("'area_codes' field removed and save has been saved!")
