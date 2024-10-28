0a378fdb79a371712511a41d7493a390cea71272
TheRealCambu <s324564@studenti.polito.it>
2024-10-28 12:11:34 +0100

    Arrived up to exercise 3 of Lab2

--- Lab2/exercise_set1..py
+++ Lab2/exercise_set1..py
@@ -15,12 +15,32 @@ def exercise1(file_path):
     plt.show()
 
 
-def exercise2():
-    pass
+def exercise2(file_path):
+    df = pd.read_csv(file_path)
+    list_of_months = list(calendar.month_name)[1:]
+    fig, ax = plt.subplots()
+    ax.plot(df["month_number"], df["total_profit"], label="Profit data of last year", color="r",
+            marker="o", markerfacecolor="k", linestyle="-", linewidth=3)
+    fig.suptitle("Total Profit for each Month of the Year")
+    ax.set_xticks(df["month_number"])
+    ax.set_xticklabels(list_of_months, rotation=60)
+    ax.yaxis.set_major_formatter('${x:.0f}')
+    fig.subplots_adjust(bottom=0.2, left=0.15)
+    plt.grid()
+    plt.show()
 
 
-def exercise3():
-    pass
+def exercise3(file_path):
+    df = pd.read_csv(file_path)
+    months = df.iloc[:, 0]
+    fig, ax = plt.subplots(figsize=(12, 8))
+    fig.suptitle("Product sales data for each month", fontsize=20)
+    for column in df.columns[1:-2]:
+        ax.plot(months, df[column], label=column, linewidth=2)
+
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 
 def exercise4():
@@ -40,16 +60,16 @@ def exercise7():
 
 
 if __name__ == "__main__":
-    print("EXERCISE SET 1")
-    print("EX1")
-    exercise1("sales_data.csv")
+    # print("EXERCISE SET 1")
+    # print("EX1")
+    # exercise1("sales_data.csv")
 
     # print("EX2")
-    # exercise2()
-    #
-    # print("EX3")
-    # exercise3()
-    #
+    # exercise2("sales_data.csv")
+
+    print("EX3")
+    exercise3("sales_data.csv")
+
     # print("EX4")
     # exercise4()
     #
